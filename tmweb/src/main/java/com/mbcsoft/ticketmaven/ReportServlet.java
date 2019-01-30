61185fb23cb51593c23c64bf13eb0990287ab955
Mike Berger <mike@mbcsoft.com>
2019-01-30 15:29:30 -0500

    add jasper report servlet

---
+++ tmweb/src/main/java/com/mbcsoft/ticketmaven/ReportServlet.java
@@ -0,0 +1,98 @@
+package com.mbcsoft.ticketmaven;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+
+import javax.ejb.EJB;
+import javax.naming.InitialContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+
+import com.mbcsoft.ticketmaven.ejb.CustomerBean;
+
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.util.JRLoader;
+
+/**
+ * Servlet implementation class ReportServlet
+ */
+@WebServlet("/ReportServlet")
+public class ReportServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	@EJB private CustomerBean rbean;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public ReportServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+		String id = request.getParameter("id");
+
+		JasperPrint jasperPrint = null;
+		try {
+			
+			int instance = rbean.getCurrentCustomer().getInstance().getRecordId();
+			String reportname = "na";
+			HashMap<String, Object> parms = new HashMap<String, Object>();
+
+			if( "cust".equals(id) )
+			{
+				reportname = "customer";
+				parms.put("instance", Integer.valueOf(instance));
+			}
+			
+			InputStream is = ReportServlet.class.getResourceAsStream("/" + reportname + ".jasper");
+
+			String targetFileName = reportname + ".pdf";
+			JasperReport jasperReport = (JasperReport) JRLoader.loadObject(is);
+			InitialContext ctx = new InitialContext();
+			DataSource ds = (DataSource) ctx.lookup("java:/tm/tmdb");
+			
+			jasperPrint = JasperFillManager.fillReport(jasperReport, parms, ds.getConnection());
+			
+			ServletOutputStream outputstream = response.getOutputStream();
+			ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+			JasperExportManager.exportReportToPdfStream(jasperPrint, byteArrayOutputStream);
+			response.setContentType("application/pdf");
+			outputstream.write(byteArrayOutputStream.toByteArray());
+			response.setHeader("Cache-Control", "max-age=0");
+			response.setHeader("Content-Disposition", "attachment; filename=" + targetFileName);
+			outputstream.flush();
+			outputstream.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
