da6d60f39c971a21471dc4aedf1bded8dbb8d4e1
Mike Berger <mike@mbcsoft.com>
2019-04-11 19:10:46 -0400

    switch to GPL3

--- tmserviceapi/src/main/java/com/mbcsoft/ticketmaven/ejb/ZoneBean.java
+++ tmserviceapi/src/main/java/com/mbcsoft/ticketmaven/ejb/ZoneBean.java
@@ -1,14 +1,19 @@
 /*******************************************************************************
- * MBCSoft License
- * 
- * Copyright (C) 2019, Michael Berger
- * 
- * All Rights Reserved
- * 
- * PROPRIETARY
- * 
- * This Software shall not be in the possession of, distributed to, or routed to anyone except with written permission of Michael Berger.
- ******************************************************************************/
+ * Copyright (C) 2019 Mike Berger
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *******************************************************************************/
 package com.mbcsoft.ticketmaven.ejb;
 
 
