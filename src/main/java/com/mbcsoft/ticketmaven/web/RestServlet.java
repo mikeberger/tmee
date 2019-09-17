70e78cb8cc3ebd551ae0643a84a75d4701050005
Mike Berger <i_flem6@hotmail.com>
2019-09-17 12:42:51 -0400

    add license headers, about page

--- src/main/java/com/mbcsoft/ticketmaven/web/RestServlet.java
+++ src/main/java/com/mbcsoft/ticketmaven/web/RestServlet.java
@@ -16,6 +16,28 @@
  *******************************************************************************/
 package com.mbcsoft.ticketmaven.web;
 
+/*-
+ * #%L
+ * tmee
+ * %%
+ * Copyright (C) 2019 Michael Berger
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
+ */
+
 import java.net.HttpURLConnection;
 import java.sql.Timestamp;
 import java.util.ArrayList;
@@ -222,7 +244,7 @@ public class RestServlet {
 
 	private void loadCusts( ArrayList<Zone> zonelist) {
 
-		for (int i = 1; i < 250; i++) {
+		for (int i = 1; i < 25; i++) {
 			Faker faker = new Faker();
 
 			Customer c = new Customer();
